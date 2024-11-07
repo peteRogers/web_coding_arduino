62d5f34cd20115563c7c1e01c1526c6654cb881e
Peter Rogers <p.rogers@gold.ac.uk>
2024-11-07 00:17:15 +0000

    changed share

--- SerialExperiment/sketch.js
+++ SerialExperiment/sketch.js
@@ -1,4 +1,4 @@
-let inVal;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -8,6 +8,6 @@ function setup() {
 
 function draw() {
   background(255);
-  inVal = readArduinoVal();
-  circle(width/2, height/2, inVal);
+  readArduino();
+  circle(width/2, height/2, arduinoVals[0]);
 }
--- sharedFiles/arduinoConn.js
+++ sharedFiles/arduinoConn.js
@@ -1,16 +1,16 @@
 let connectBtn;
 let port;
+let arduinoVals = [];
 
 function connectArduino(){
   port = createSerial();
   let usedPorts = usedSerialPorts();
   if (usedPorts.length > 0) {
-    port.open(usedPorts[0], 57600);
+    port.open(usedPorts[0], 9600);
   }else{
     connectBtn = createButton('Connect to Arduino');
     connectBtn.position(80, 200);
     connectBtn.mousePressed(connectBtnClick);
-   
   }
 }
 
@@ -23,18 +23,13 @@ function parseStringToIntArray(str) {
     .map(num => parseInt(num, 10)); // Convert each string to an integer
 }
 
-function readArduinoVals(){
+function readArduino(){
   let inString = port.readUntil("\n");
-  port.clear();
-  let vals = parseStringToIntArray(inString);
-  return vals;
-}
-
-function readArduinoVal(){
-  let inString = port.readUntil("\n");
-  port.clear();
-  let val = parseStringToIntArray(inString);
-  return val[0];
+  //port.clear(); not necessary on uno
+  if(inString != ""){
+    let vals = parseStringToIntArray(inString);
+    arduinoVals = vals;
+  }
 }
 
 function connectBtnClick() {
@@ -50,9 +45,3 @@ function sendDataToArduino(message){
   port.write("\n");
 
 }
-
-
-
-// function sendBtnClick() {
-//   port.write("Hello from p5.js\n");
-// }
