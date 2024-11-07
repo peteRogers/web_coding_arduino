1e5b4bb4a1b8758c041751c0197c829bc9380c88
Peter Rogers <p.rogers@gold.ac.uk>
2024-11-07 01:05:07 +0000

    d

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web_coding_arduino
  
+### Link to p5 web editor examples
+[Serial Arduino example](https://editor.p5js.org/peteRogers/sketches/mrsVJQji6)
+
+[Serial Arduino with ml5.js face recognition](https://editor.p5js.org/peteRogers/sketches/mrsVJQji6)
--- faceCam_Stepper/sketch.js
+++ faceCam_Stepper/sketch.js
@@ -2,12 +2,8 @@ let faceMesh;
 let video;
 let faces = [];
 
-let inVal; 
-
-
 function preload() {
   faceMesh = ml5.faceMesh();
- 
 }
 
 function setup() {
@@ -23,11 +19,10 @@ function draw() {
   background(220);
   image(video, 0, 0);
   if(faces.length > 0){
-    //this gets the 
     let fx = faces[0].box.xMin+(faces[0].box.width/2)
     circle(fx, height/2, 20, 20);
-    let forArduino = map(fx, 0, width, 0, 2048);
-    sendDataToArduino(forArduino);
+    let facePos = map(fx, 0, width, 0, 2048);
+    sendDataToArduino(facePos);
   }
 }
 
